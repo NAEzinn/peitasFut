b2f56cce03ac7b022ed2a3c1b6c0773053d47e3f
Gabriel da Silva <gabrieldasilvaribeiro181@gmail.com>
2024-10-30 14:46:44 -0300

    30/10/2024

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggleButton');
+const closeButton = document.getElementById('closeButton');
+const navbarNav = document.getElementById('navbarNav');
+
+toggleButton.addEventListener('click', ()=> {
+    navbarNav.classList.toggle('open');
+});
+
+closeButton.addEventListener('click', () =>{
+    navbarNav.classList.remove('open');
+})
